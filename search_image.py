426b457808d241b452061c4b47c6688c6201d354
farisskhairy <khairyf@oregonstate.edu>
2025-02-12 18:37:14 -0800

    Feb 12, 25 - Updated code.

--- search_image.py
+++ search_image.py
@@ -4,7 +4,7 @@ from googleapiclient.discovery import build
 def show_image(search):
     search_google = build("customsearch", "v1", developerKey = "")
 
-    results = (search_google.cse().list(q = search, cx = "", num = 1).execute())
+    results = (search_google.cse().list(q = search, fileType = ".jpg", num = 1).execute())
 
     image_link = results["items"][0]["pagemap"]["cse_image"][0]["src"]
 
